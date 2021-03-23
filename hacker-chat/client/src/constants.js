f4e3f3db6844c344f396597b3cee2f0ec3a879f4
Jorge <jorgews.dev@gmail.com>
2021-03-22 22:41:25 -0300

    build: 🏠 - Init project and creating client side logic to render informations on console.

---
+++ hacker-chat/client/src/constants.js
@@ -0,0 +1,14 @@
+export const constantes = {
+    events: {
+        app: {
+            MESSAGE_RECEIVED: 'message:received',
+            MESSAGE_SENT: 'message:sent',
+            ACTIVITY_LOG_UPDATED: 'activityLog:updated',
+            STATUS_UPDATED: 'status:updated',
+        },
+
+        socket: {
+
+        }
+    }
+}
