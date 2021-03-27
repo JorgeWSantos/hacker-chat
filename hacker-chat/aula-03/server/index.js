fa95f1dbaa753c88c88831b0b968d2c6716ebd87
Jorge <jorgews.dev@gmail.com>
2021-03-27 19:13:51 -0300

    feat: 🎸 Add logic to vinculate events to functions and create updateUsers/JoinRoom logic.

---
+++ hacker-chat/aula-03/server/index.js
@@ -0,0 +1,19 @@
+import SocketServer from "./src/socket.js";
+import Event from 'events'
+import { constants } from "./src/constants.js";
+import Controller from './src/controller.js';
+
+const eventEmitter = new Event();
+const port = process.env.PORT || 9898;
+
+
+
+const socketServer = new SocketServer({port});
+const server = await socketServer.initialize(eventEmitter);
+console.log('socket server is running at', server.address().port);
+const controller = new Controller({ socketServer });
+
+eventEmitter.on(
+    constants.event.NEW_USER_CONNECTED, 
+    controller.onNewConnection.bind(controller)
+);
