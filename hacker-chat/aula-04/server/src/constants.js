9fe78d0ca43fca4eda5b3a2fd2c9e099a5661e64
Jorge <jorgews.dev@gmail.com>
2021-03-27 20:15:47 -0300

    refact: 💡- Cleaning folders.

--- hacker-chat/aula-04/server/src/constants.js
+++
@@ -1,8 +0,0 @@
-export const constants = {
-    event: {
-        MESSAGE: 'message',
-        NEW_USER_CONNECTED: 'newUserConnected',
-        DICONNECT_USER: 'disconnectUser',
-        UPDATE_USERS: 'updateUsers'
-    }
-}
